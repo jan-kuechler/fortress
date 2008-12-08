095eacb405172688693b8396d5fa7c5e02acc80c
borlox <kuchen_@gmx.de>
2008-12-08 18:18:08 +0100

    - Warning for missing .type attribute is only active in alpha versions now.

--- Core.lua
+++ Core.lua
@@ -429,9 +429,15 @@ function Fortress:LibDataBroker_DataObjectCreated(event, name, obj)
 	Debug("Dataobject Registered:", name)
 	
 	local t = obj.type
+	
+	-- Print warnings in alpha versions.
+	-- Maybe some experienced users can get the developers of plugins to follow the spec.
+	--@alpha@
 	if t == nil then
 		print("Fortress: The data object", name, "has no type attribute set. Please report this to the author of the plugin.")
 	end
+	--@end-alpha@
+	
 	-- support data objects without type set, allthough that's not correct
 	if t and (t ~= "data source" and t ~= "launcher") then
 		return
